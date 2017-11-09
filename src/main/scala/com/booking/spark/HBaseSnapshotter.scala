f3c5371886060ce612fefcb020dbd7e7dac85c78
Quan Do <quan.dm@teko.vn>
2017-11-09 10:07:24 +0000

    update

--- src/main/scala/com/booking/spark/HBaseSnapshotter.scala
+++ src/main/scala/com/booking/spark/HBaseSnapshotter.scala
@@ -2,33 +2,24 @@ package com.booking.spark
 
 import com.booking.sql.{DataTypeParser, MySQLDataType}
 import java.io.File
+import java.nio.charset.Charset
 import java.util.NavigableMap
-import java.sql.{Timestamp,Date}
+import java.sql.{Date, Timestamp}
 import java.text.SimpleDateFormat
+
 import org.apache.log4j.{Level, Logger}
 import com.google.gson.{JsonObject, JsonParser}
 import org.apache.hadoop.hbase.spark.HBaseContext
-import org.apache.hadoop.hbase.{TableName, HBaseConfiguration}
-import org.apache.hadoop.hbase.client.{Result, Scan, Get, HTable}
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
+import org.apache.hadoop.hbase.client.{Get, HTable, Result, Scan}
 import org.apache.hadoop.hbase.filter.{FilterList, FirstKeyOnlyFilter, KeyOnlyFilter}
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{Row, SaveMode, SQLContext}
+import org.apache.spark.sql.{Row, SQLContext, SaveMode}
 import org.apache.spark.sql.hive.HiveContext
-import org.apache.spark.sql.types.
-  {
-    StructField,
-    StructType,
-    DataType,
-    DateType,
-    DoubleType,
-    IntegerType,
-    LongType,
-    StringType,
-    TimestampType
-  }
+import org.apache.spark.sql.types.{DataType, DateType, DoubleType, IntegerType, LongType, StringType, StructField, StructType, TimestampType}
 
 /**
   * Spark application that takes a snapshot of an HBase table at a
@@ -73,11 +64,12 @@ object HBaseSnapshotter {
             val fieldValue: String = Bytes.toStringBinary(familyMap.get(Bytes.toBytes(field.name)))
 
             if (fieldValue.toUpperCase() == "NULL")
-              "NULL"
+              null
             else {
               field.dataType match {
                 case IntegerType => fieldValue.toInt
                 case LongType => fieldValue.toLong
+                case DoubleType => fieldValue.toDouble
                 case DoubleType => fieldValue.toDouble
                 case TimestampType => new java.sql.Timestamp(fieldValue.toLong)
                 // TODO(psilva): This is a NOOP for now, until
@@ -88,15 +80,12 @@ object HBaseSnapshotter {
                   val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
                   new java.sql.Date(sdf.parse(fieldValue).getTime());
                 }
-                case _ => fieldValue
+                case _ => Bytes.toString(familyMap.get(Bytes.toBytes(field.name)))
               }
             }
           }
           catch {
             case e: Exception => {
-              logger.error(field.toString())
-              logger.error(familyMap.toString())
-              logger.error(e.toString())
               throw e
             }
           }
@@ -132,18 +121,12 @@ object HBaseSnapshotter {
 
     val rowRDD = hbaseRDD.map({ r => transformMapToRow(r, schema, true) })
 
-    logger.debug(rowRDD)
-    logger.debug("test")
    // val sqlContext = new SQLContext(sc)
-    // System.exit(-1)
 
-    //val dataFrame = hc.createDataFrame(rowRDD, schema)
     val dataFrame = hc.createDataFrame(rowRDD, schema)
 
     //dataFrame.registerTempTable("wikistats")
-
     //val group_res = sqlContext.sql("select * from wikistats limit 10")
-
     //group_res.show()
 
 
